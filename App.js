dab3c6cc4188400c73451a58eb47e13dbdfbe168
cyio <icaner@qq.com>
2018-07-11 13:10:19 +0800

    update

--- App.js
+++ App.js
@@ -4,54 +4,57 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
   View,
   Button,
-  PanResponder
-} from 'react-native';
-import VideoPlayer from 'react-native-video-controls';
-import DeviceBrightness from 'react-native-device-brightness';
+  PanResponder,
+} from 'react-native'
+import VideoPlayer from 'react-native-video-controls'
+import DeviceBrightness from 'react-native-device-brightness'
 
 export default class App extends Component<{}> {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       paused: true,
       bg: 'white',
       top: 0,
       left: 0,
-      brightness: 1
+      brightness: 1,
+      // url: 'http://pub.dotabocai.com/sample-small.mp4',
+      url: 'https://vjs.zencdn.net/v/oceans.mp4',
     }
   }
 
-  setBrightness (n) {
+  setBrightness(n) {
     DeviceBrightness.setBrightnessLevel(n)
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this._panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponder: ()=> true,
-      onPanResponderGrant: ()=>{
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
         this._top = this.state.top
         this._left = this.state.left
-        this.setState({bg: 'red'})
+        this.setState({ bg: 'red' })
       },
-      onPanResponderMove: (evt,gs)=>{
-        console.log(gs.dx+' '+gs.dy)
+      onPanResponderMove: (evt, gs) => {
+        console.log(gs.dx + ' ' + gs.dy)
         this.setState({
-          top: this._top+gs.dy,
-          left: this._left+gs.dx
+          top: this._top + gs.dy,
+          left: this._left + gs.dx,
         })
       },
-      onPanResponderRelease: (evt,gs)=>{
+      onPanResponderRelease: (evt, gs) => {
         this.setState({
           bg: 'white',
-        })}
+        })
+      },
     })
   }
 
@@ -60,9 +63,8 @@ export default class App extends Component<{}> {
       <View style={styles.container}>
         <View style={[styles.playerWrapper]}>
           <VideoPlayer
-            source={{ uri: 'https://vjs.zencdn.net/v/oceans.mp4' }}
+            source={{ uri: this.state.url }}
             paused={this.state.paused}
-            volume={0.5}
             setBrightness={this.setBrightness.bind(this)}
           />
         </View>
@@ -77,18 +79,18 @@ export default class App extends Component<{}> {
           />
         </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+  container: {},
   playerWrapper: {
     backgroundColor: '#000',
     height: 220,
   },
-  video: { // 必须，否则会看不到视频
+  video: {
+    // 必须，否则会看不到视频
     position: 'absolute',
     top: 0,
     right: 0,
@@ -100,21 +102,21 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-	rect: {
+  rect: {
     position: 'absolute',
     top: 30,
     backgroundColor: 'red',
     right: 10,
-		width: 60,
-		height: 140,
-		borderWidth: 1,
-		borderColor: 'black'
-	}
-});
-          // <VideoPlayer
-            // source={{ uri: 'https://vjs.zencdn.net/v/oceans.mp4' }}
-          // />
-					// <View
-						// {...this._panResponder.panHandlers}
-            // style={styles.rect}>
-          // </View>
+    width: 60,
+    height: 140,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+})
+// <VideoPlayer
+// source={{ uri: 'https://vjs.zencdn.net/v/oceans.mp4' }}
+// />
+// <View
+// {...this._panResponder.panHandlers}
+// style={styles.rect}>
+// </View>
